7fd202c90037f3685c33d6fd54357ffef2bed932
wwpeps <aboba1333333@gmail.com>
2024-09-17 11:14:38 +0500

    добавлена сортировка индексов

--- pr03/MainWindow.xaml.cs
+++ pr03/MainWindow.xaml.cs
@@ -23,7 +23,9 @@ namespace pr03
     {
 
         private static List<user> users = new List<user>();
-        
+        private static int[] inde = new int[5];
+
+
 
         public MainWindow()
         {
@@ -32,15 +34,21 @@ namespace pr03
 
         void Load() 
         {
+            int v = 1;
             users.Add(new user
             {
+                id = Convert.ToInt32(tx.Text),
                 name = ta.Text,
                 password = tb.Text,
             });
+            inde[v] = Convert.ToInt32(tx.Text);
+
+            v += 1;
         }
 
         public class user 
         { 
+            public int id { get; set; }
             public string name { get; set; }
             public string password { get; set; }
         }
@@ -48,15 +56,69 @@ namespace pr03
         private void ba_Click(object sender, RoutedEventArgs e)
         {
             Load();
-            foreach (var user in users) 
+            foreach (var item in users) 
             { 
-                lia.Items.Add(user);
+                lia.Items.Add(item);
+            }
+        }
+       
+
+
+        private void ta_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+
+        public static void Quicksort(int[] elements, int left, int right)
+        {
+            int i = left;
+            int j = right;
+
+            int pivot = elements[(left + right) / 2];
+
+            while (i <= j)
+            {
+                while (elements[i].CompareTo(pivot) < 0)
+                {
+                    i++;
+                }
+                while (elements[j].CompareTo(pivot) > 0)
+                {
+                    j--;
+                }
+
+
+                if (i <= j)
+                {
+                    // Swap
+                    int tmp = elements[i];
+                    elements[i] = elements[j];
+                    elements[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+            // Recursive calls
+            if (left < j)
+            {
+                Quicksort(elements, left, j);
+            }
+            if (i < right)
+            {
+                Quicksort(elements, i, right);
             }
         }
 
-        public List<> SortQuick(List<users>, int li, int ri) 
-        { 
-        
+        private void sfb_Click(object sender, RoutedEventArgs e)
+        {
+            Quicksort(inde, 0, inde.Length - 1);
+            foreach (var item in inde.ToList())
+            {
+                lia.Items.Add(item);
+            }
         }
     }
 }
