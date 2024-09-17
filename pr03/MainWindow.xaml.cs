b86f5e11ecfb4f5ab12c872e25ea329487eef963
Данил Хуснутдинов < @42-13>
2024-09-17 10:00:28 +0500

    Добавьте файлы проекта.

---
+++ pr03/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pr03
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private static List<user> users = new List<user>();
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void Load() 
+        {
+            users.Add(new user
+            {
+                name = ta.Text,
+                password = tb.Text,
+            });
+        }
+
+        public class user 
+        { 
+            public string name { get; set; }
+            public string password { get; set; }
+        }
+
+        private void ba_Click(object sender, RoutedEventArgs e)
+        {
+            Load();
+            foreach (var user in users) 
+            { 
+                lia.Items.Add(user);
+            }
+        }
+
+        public List<> SortQuick(List<users>, int li, int ri) 
+        { 
+        
+        }
+    }
+}
